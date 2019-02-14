52ceca38b345327c65cdbd5dbde053d28d1fa206
Robert <robert.pakkanen@helsinki.fi>
2019-02-14 16:34:41 +0200

    Tehtävät 4.13-4.14

--- blogilista-backend/controllers/blogs.js
+++ blogilista-backend/controllers/blogs.js
@@ -28,4 +28,28 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes === undefined ? 0 : body.likes
+  }
+
+  try { 
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog.toJSON())
+  } catch(exception) {
+    next(exception)
+  }
+
+})
+
 module.exports = blogsRouter
